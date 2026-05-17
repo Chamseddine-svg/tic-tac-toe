b1409efb0589630ee201a20c30e9235f480398b2
Chams Eddine Khlifi <khlifichamseddine@gmail.com>
2026-05-17 02:50:15 +0100

    add win check and minor fixes

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const gameBoard = (function(){
     const board = ['','','','','','','','','']
     
     function reset(){
-        board.forEach((e)=>board[board.indexOf(e)] = '') 
+        board.forEach((e)=>board[e] = '') 
     }
 
     function setMarker(marker,position){
@@ -35,6 +35,17 @@ function Player(name,marker,score = 0){
 }
 
 //game runner
+const winningCombination = [
+    [0,1,2], // top row
+    [3,4,5], // middle row
+    [6,7,8], // bottom row
+    [0,3,6], // left column
+    [1,4,7], // middle column
+    [2,5,8], // right column
+    [0,4,8], // diagonal top-left to bottom-right
+    [2,4,6]  // diagonal top-right to bottom-left
+]
+
 function gameRunner(player1,player2){
     let round = 0 //round is a singe player move so game have 9 rounds as the tictac grid 
     const {reset,setMarker,getMarker} = gameBoard
@@ -43,7 +54,19 @@ function gameRunner(player1,player2){
         return getMarker(position) === ''
     }
 
-    function currenPlayer(){
+    function checkWin(){
+    const marker = currentPlayer().marker
+    
+    // Check if ANY combination is a winner
+    return winningCombination.some((combination) => {
+        // Check if ALL elements in this combination match the marker
+        return combination.every((position) => {
+            return getMarker(position) === marker
+        })
+    })
+}
+
+    function currentPlayer(){
         return round%2 === 0 ? player1 : player2 
     }
 
@@ -53,7 +76,7 @@ function gameRunner(player1,player2){
 
     function makeMove(position){
         let validGame = true
-        if (currenPlayer() === player1){
+        if (currentPlayer() === player1){
             log(`Player1`)
             checkValidMove(position) ? (setMarker(player1.marker , position)) : validGame = false ;
         } else {
